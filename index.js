9c0814d3f548f71e874df9e7bd1e2540a0f98cfe
jtPox <me@jtpox.com>
2018-09-07 19:43:18 +0800

    Test

--- index.js
+++ index.js
@@ -1,41 +1,15 @@
-import gpio from 'pigpio';
+import Rpio from 'rpio';
 
-const Gpio = gpio.Gpio;
+/*
+ * LP293D Pin-outs
+ * GPIO25 (Pin 22) -> 1 (VCC)
+ * GPIO24 (Pin 18) -> 2 (Input1)
+ * GPIO23 (Pin 16) -> 7 (Input2)
+ */
+Rpio.open(25, Rpio.OUTPUT, Rpio.LOW); //Enable
 
-// The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
-/* const MICROSECDONDS_PER_CM = 1e6 / 34321;
-
-const trigger = new Gpio(21, { mode: Gpio.OUTPUT });
-const echo = new Gpio(20, { mode: Gpio.INPUT, alert: true });
-
-trigger.digitalWrite(0); // Make sure trigger is low
-
-const watchHCSR04 = () => {
-  let startTick;
-
-  echo.on('alert', (level, tick) => {
-    if (level == 1) {
-      startTick = tick;
-    } else {
-      const endTick = tick;
-      const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
-      console.log(diff / 2 / MICROSECDONDS_PER_CM);
-    }
-  });
-};
-
-watchHCSR04();
-
-// Trigger a distance measurement once per second
-setInterval(() => {
-  trigger.trigger(10, 1); // Set trigger high for 10 microseconds
-}, 1000); */
-
-const enable = new Gpio(25, { mode: Gpio.OUTPUT });
-const forward = new Gpio(24, { mode: Gpio.OUTPUT });
-
-
-setInterval(() => {
-  enable.pwmWrite(0);
-  forward.servoWrite(1000);
-}, 1000);
+// Moving forward.
+Rpio.write(25, Rpio.HIGH);
+Rpio.open(24, Rpio.OUTPUT, Rpio.low);
+Rpio.open(23, Rpio.OUTPUT, Rpio.HIGH);
+Rpio.sleep(5);
