b4f00d1a1fe2a9fc4c82d7f90ef2d201aa7cb7fa
jtPox <me@jtpox.com>
2018-09-08 18:30:22 +0800

    Test

--- app/init.js
+++ app/init.js
@@ -7,8 +7,14 @@ class Init {
      * Setting interval to test out sensor every second.
      */
     // setInterval(this.interval.bind(this.sensor), 1000);
+    this.motor.forward();
     setInterval(() => {
       console.log(this.sensor.distance);
+      if (this.sensor.distance < 18) { // Less than 18cm for testing.
+        this.motor.stop();
+      } else {
+        this.motor.forward();
+      }
     }, 2000);
   }
 
--- app/module/motor.js
+++ app/module/motor.js
@@ -1,3 +1,4 @@
+import Rpio from 'rpio';
 class Motor {
   /*
    * Uses the LP293 Driver.
@@ -6,6 +7,29 @@ class Motor {
     this.vcc = vcc;
     this.input1 = input1;
     this.input2 = input2;
+
+    /*
+     * Setting up GPIO
+     */
+    Rpio.open(this.vcc, Rpio.OUTPUT);
+    Rpio.open(this.input1, Rpio.OUTPUT);
+    Rpio.open(this.input2, Rpio.OUTPUT);
+  }
+
+  forward() {
+    Rpio.write(this.input2, Rpio.HIGH);
+    Rpio.write(this.input1, Rpio.LOW);
+    Rpio.wrote(this.vcc, Rpio.HIGH);
+  }
+
+  reverse() {
+    Rpio.write(this.input2, Rpio.LOW);
+    Rpio.write(this.input1, Rpio.HIGH);
+    Rpio.wrote(this.vcc, Rpio.HIGH);
+  }
+
+  stop() {
+    Rpio.write(this.vcc, Rpio.LOW);
   }
 }
 
