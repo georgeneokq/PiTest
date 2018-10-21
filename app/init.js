896182507c4dfe46a353b73bc0603deecc0963f4
George <george.neokq@gmail.com>
2018-10-22 03:16:27 +0800

    Changes

--- app/init.js
+++ app/init.js
@@ -11,13 +11,13 @@ class Init {
     // setInterval(this.interval.bind(this.sensor), 1000);
     this.motor.forward();
     
-    // Check if distance is lesser than the maximum allowed distance
-    this.maxDistance = 18; // in cm
+    // Check if distance is lesser than the allowed distance
+    this.lowestAllowedDistance = 18; // in cm
     
     this.sensor.on('distancechanged', () => {
-      
+
       // Stop motor if distance of object exceeds the specified max distance
-      if(this.sensor.distance > this.maxDistance) {
+      if(this.sensor.distance < this.lowestAllowedDistance) {
         this.motor.stop();
         console.log(`Motor stopped. Distance: ${this.sensor.distance}`);
       } else {
